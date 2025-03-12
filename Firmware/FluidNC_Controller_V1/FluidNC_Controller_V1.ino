83dffa7275671c8b10b095c999927ef751bcc29b
Th3rd <thethirdcreator@gmail.com>
2025-03-12 23:06:46 +0300

    - Добавлены файлы с описанием алгоритмов и класса cnc

--- Firmware/FluidNC_Controller_V1/FluidNC_Controller_V1.ino
+++ Firmware/FluidNC_Controller_V1/FluidNC_Controller_V1.ino
@@ -16,18 +16,18 @@ volatile uint64_t isrCounter = 0;
 volatile char key;
 
 void ARDUINO_ISR_ATTR onTimer() {
-// key = keypad.getKey();
+  // key = keypad.getKey();
 
-// newCall = millis();
+  // newCall = millis();
   // if ((newCall - lastCall) > 100){
   // // portENTER_CRITICAL_ISR(&displayMux);
   // draw();
   // // portEXIT_CRITICAL_ISR(&displayMux);
   // }
 
-// lastCall = millis();
+  // lastCall = millis();
 
-isrCounter++;
+  isrCounter++;
 }
 
 
@@ -44,14 +44,13 @@ void setup() {
   BaseTimer = timerBegin(1000);
   timerAttachInterrupt(BaseTimer, &onTimer);
   timerAlarm(BaseTimer, 100, true, 0);
-
 }
 
 void loop() {
 
   // displayTime = millis();
   // if ((millis() - lastCall) > 100)
-  draw(); //12864 display only
+  draw();  //12864 display only
   // lastCall = millis();
   // Serial.print("Draw time: ");
   // Serial.println((millis() - displayTime));
@@ -73,9 +72,9 @@ void draw() {
 
     u8g2.setFont(u8g2_font_unifont_t_symbols);
     u8g2.setCursor(0, 0);
-    u8g2.drawGlyph(5, 20, 0x2603); // снеговик
+    u8g2.drawGlyph(5, 20, 0x2603);  // снеговик
     u8g2.setCursor(0, 20);
-    u8g2.drawGlyph(5, 60, 0x2615); // Кофе
+    u8g2.drawGlyph(5, 60, 0x2615);  // Кофе
 
     u8g2.setCursor(50, 50);
     u8g2.print(isrCounter);
@@ -124,20 +123,16 @@ void jog(int dir) {
   Serial1.write(0x0A);
 }
 
-#define B_COORD_ABS 0 //удалить
-#define B_COORD_REL 0 //удалить
-void keypadEvent(KeypadEvent key) 
-{
-  switch (keypad.getState()) 
-  {
+#define B_COORD_ABS 0  //удалить
+#define B_COORD_REL 0  //удалить
+void keypadEvent(KeypadEvent key) {
+  switch (keypad.getState()) {
     case PRESSED:
-    {
+      {
         Serial.println(key);  // Дебаг для вывода нажатой кнопки
-        if (Fence.b_isHomed) 
-        {
+        if (Fence.b_isHomed) {
           // isHomed == Ok-----------------------------------------
-          if (key >= '0' && key <= '9') 
-          {
+          if (key >= '0' && key <= '9') {
             if ((Fence.inputPos.length() <= 4))
               Fence.inputPos += key;
 
@@ -145,22 +140,21 @@ void keypadEvent(KeypadEvent key)
               Fence.inputPos = "";
 
             Serial.println(Fence.inputPos);
-          } 
-          
+          }
+
           if (key == KPD_AST)
             Fence.inputPos += '.';
 
-          switch (key) 
-          {
+          switch (key) {
             case KPD_F1:
-              Fence.inputPos = "0"; //Set position to home
+              Fence.inputPos = "0";  //Set position to home
               setPosition(0, B_COORD_ABS);
               break;
             case KPD_F2:
               Fence.changeStatus(FENCE_HOMED_OK);
               break;
             case KPD_ESC:
-                
+
               break;
             case KPD_ENT:  //OK
               setPosition(0, B_COORD_ABS);
@@ -174,18 +168,17 @@ void keypadEvent(KeypadEvent key)
             default:
               break;
           }
-         // isHomed == Ok-----------------------------------------   
+          // isHomed == Ok-----------------------------------------
         }
-    }
-  
+      }
+
 
 
     case RELEASED:
       break;
 
     case HOLD:
-      switch (key) 
-      {
+      switch (key) {
         case KPD_F1:
           {
             Fence.changeStatus(FENCE_HOMED_OK);
@@ -207,8 +200,8 @@ void keypadEvent(KeypadEvent key)
             Serial.println("Jog rigth");
             break;
           }
-         case KPD_ESC:
-          { 
+        case KPD_ESC:
+          {
             Fence.inputPos = "";
             break;
           }
